1c45bb4a51841d61625eecd64f1ef90329c8acf0
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-06-16 15:56:19 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,3 @@
-git clone https://github.com/vexido/Man-Userbot /root/vexido-man
-cp CREDS/config.env /root/vexido-man/config.env
-cd /root/vexido-man
-docker build . -t man
-docker run --name ub man
+cd CREDS
+docker build . -t bene
+docker run bene
